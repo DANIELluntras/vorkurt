b735af912672666c05368aaa9edad7b11d566094
NsdHSO <nechiforelsamuel@yahoo.com>
2021-12-10 10:52:59 +0200

    chore: prepare for migration angular 13

--- src/app/module/transport/item/components/list-items/list-items.component.ts
+++ src/app/module/transport/item/components/list-items/list-items.component.ts
@@ -5,19 +5,20 @@ export interface PeriodicElement {
   position: number;
   weight: number;
   symbol: string;
+  weighte: number
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', weighte: 1 },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', weighte: 1  },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', weighte: 1  },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', weighte: 1  },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B', weighte: 1  },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C', weighte: 1  },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' , weighte: 1 },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' , weighte: 1 },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F', weighte: 1  },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne', weighte: 1  },
 ];
 
 @Component({
